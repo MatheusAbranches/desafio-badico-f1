e4010bcd07af13d228873341087746135fc1b18c
Matheus Abranches <matheus-abranches@hotmail.com>
2022-11-21 15:05:15 -0300

    expandbar finish

--- src/components/ExpandBar/ExpandBar.js
+++ src/components/ExpandBar/ExpandBar.js
@@ -2,40 +2,40 @@ import React from "react";
 import MaxExpand from "../../img/pilots/max.png";
 import "./ExpandBar.css";
 
-const ExpandBar = () => {
+const ExpandBar = ({src, team, Country, podiuns, points, GrandPix, worldChampion}) => {
   return (
     <div className="container_expand_bar">
-      <img src={MaxExpand} alt="" />
+      <img src={src} alt="" />
       <div className="content_pilots_info">
       <div className="pilot_info">
         <span className="pilot_info_option">Team</span>
 
-        <span className="pilot_info_result">RedBull Racing</span>
+        <span className="pilot_info_result">{team}</span>
       </div>
       <div className="pilot_info">
         <span className="pilot_info_option">Country</span>
 
-        <span className="pilot_info_result">Netherlands</span>
+        <span className="pilot_info_result">{Country}</span>
       </div>
       <div className="pilot_info">
         <span className="pilot_info_option">Podiuns</span>
 
-        <span className="pilot_info_result">75</span>
+        <span className="pilot_info_result">{podiuns}</span>
       </div>
       <div className="pilot_info">
         <span className="pilot_info_option">Points</span>
 
-        <span className="pilot_info_result">1948.5</span>
+        <span className="pilot_info_result">{points}</span>
       </div>
       <div className="pilot_info">
         <span className="pilot_info_option">Grand Pix Entered</span>
 
-        <span className="pilot_info_result">160</span>
+        <span className="pilot_info_result">{GrandPix}</span>
       </div>
       <div className="pilot_info">
         <span className="pilot_info_option">World Championships</span>
 
-        <span className="pilot_info_result">2</span>
+        <span className="pilot_info_result">{worldChampion}</span>
       </div>
       </div>
     </div>
--- src/pages/classification/Classification.js
+++ src/pages/classification/Classification.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Classification.css";
-import { Collapse,  } from "react-collapse";
+import { Collapse } from "react-collapse";
 import { useState } from "react";
 
 import ClassificationCard from "../../components/ClassificationCard/ClassificationCard";
@@ -9,11 +9,15 @@ import Vector from "../../img/Vector.png";
 import ClassificationBar from "../../components/ClassificationBar/ClassificationBar";
 import ExpandBar from "../../components/ExpandBar/ExpandBar";
 
+import ExpandCharles from "../../img/pilots/charles.png";
+import ExpandMax from "../../img/pilots/max.png";
+import ExpandSergio from "../../img/pilots/sergio.png";
+
 const Classification = () => {
   const [expanded1, setExpanded1] = useState(false);
   const [expanded2, setExpanded2] = useState(false);
   const [expanded3, setExpanded3] = useState(false);
-  
+
   return (
     <>
       <div className="container_title">
@@ -31,29 +35,53 @@ const Classification = () => {
           onClick={() => setExpanded1(!expanded1)}
         />
         <Collapse isOpened={expanded1}>
-          <ExpandBar/>
-        </Collapse>  
+          <ExpandBar
+            src={ExpandMax}
+            team={"RedBull Racing"}
+            Countryc={"Netherlands"}
+            podiuns={75}
+            points={1948.5}
+            GrandPix={160}
+            worldChampion={2}
+          />
+        </Collapse>
         <ClassificationBar
           position={2}
-          name={'Sergio'}
-          lastName={'PEREZ'}
-          team={'Red Bull Racing'}
+          name={"Sergio"}
+          lastName={"PEREZ"}
+          team={"Red Bull Racing"}
           points={253}
           onClick={() => setExpanded2(!expanded2)}
         />
         <Collapse isOpened={expanded2}>
-          <ExpandBar />
+          <ExpandBar
+            src={ExpandSergio}
+            team={"RedBull Racing"}
+            Country={"Mexico"}
+            podiuns={24}
+            points={1161}
+            GrandPix={233}
+            worldChampion={"N/A"}
+          />
         </Collapse>
         <ClassificationBar
           position={3}
-          name={'Charles'}
-          lastName={'LECLERC'}
-          team={'Ferrari'}
+          name={"Charles"}
+          lastName={"LECLERC"}
+          team={"Ferrari"}
           points={252}
           onClick={() => setExpanded3(!expanded3)}
         />
         <Collapse isOpened={expanded3}>
-          <ExpandBar />
+          <ExpandBar
+            src={ExpandCharles}
+            team={"Ferrari"}
+            Country={"Monaco"}
+            podiuns={23}
+            points={827}
+            GrandPix={100}
+            worldChampion={"N/A"}
+          />
         </Collapse>
       </main>
     </>
